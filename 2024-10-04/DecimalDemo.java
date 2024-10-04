1b93dee1b16b9c627b0cd2be9a8b027a58f97305
sheuyc <167731527+sheuyc@users.noreply.github.com>
2024-10-04 11:58:08 +0800

    2024-10-04

---
+++ 2024-10-04/DecimalDemo.java
@@ -0,0 +1,15 @@
+//package cc.openhome;
+
+import java.math.BigDecimal;
+
+public class DecimalDemo {
+    public static void main(String[] args) {
+        //var operand1 = new BigDecimal("1.0");
+        //var operand2 = new BigDecimal("0.8");
+        //var result = operand1.subtract(operand2);
+        BigDecimal operand1 = new BigDecimal("1.0");
+        BigDecimal operand2 = new BigDecimal("0.8");
+        BigDecimal result = operand1.subtract(operand2);
+        System.out.println(result);
+    }
+}
---
+++ 2024-10-04/Field2.java
@@ -0,0 +1,28 @@
+//package cc.openhome;
+
+class Clothes2 {
+    String color;
+    char size;
+    
+    Clothes2(String color, char size) {//Constructor 建構子，用於設定初始值
+        this.color = color;
+        this.size = size;
+    }
+    void  changeColor(String color){
+        this.color = color;
+    }
+}
+
+public class Field2 {
+    public static void main(String[] args) {
+        //var sun = new Clothes2("red", 'S');
+        //var spring = new Clothes2("green", 'M');
+        Clothes2 sun = new Clothes2("red", 'S');
+        Clothes2 spring = new Clothes2("green", 'M');
+
+        spring.changeColor("yellow");
+
+        System.out.printf("sun (%s, %c)%n", sun.color, sun.size);
+        System.out.printf("spring (%s, %c)%n", spring.color, spring.size);
+    }
+}
