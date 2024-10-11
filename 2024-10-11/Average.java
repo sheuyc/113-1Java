5a267dc7dd7376f514c01c4505b47b9e1ec2a0c4
sheuyc <167731527+sheuyc@users.noreply.github.com>
2024-10-11 11:43:16 +0800

    2024-10-11

--- 2024-10-11/Average.java
+++ 2024-10-11/Average.java
@@ -2,8 +2,8 @@
 
 public class Average {
     public static void main(String[] args) {
-        var sum = 0;
-        for(var arg : args) {
+        long sum = 0;
+        for(String arg : args) {
             sum += Long.parseLong(arg);
         }
         System.out.println("平均：" + (float) sum / args.length);
--- 2024-10-11/Sum.java
+++ 2024-10-11/Sum.java
@@ -1,16 +1,17 @@
 //package cc.openhome;
 
+//import java.awt.im.InputContext;
 import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        var console = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         
-        var sum = 0;
-        var number = 0;
+        int sum = 0;
+        int number = 0;
         do {
             System.out.print("輸入數字：");
-            number = Integer.parseInt(console.nextLine());
+            number = Integer.parseInt(input.nextLine());
             sum += number;
         } while(number != 0);
         System.out.println("總合：" + sum);
