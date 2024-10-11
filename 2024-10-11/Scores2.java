8604223da0f73d9cfe8773e6304221a9c6262513
sheuyc <167731527+sheuyc@users.noreply.github.com>
2024-10-11 10:33:31 +0800

    2024-10-11

--- 2024-10-11/Scores2.java
+++ 2024-10-11/Scores2.java
@@ -6,13 +6,21 @@ public class Scores2 {
     public static void main(String[] args) {
         var scores = new int[10];
         
-        for(var score : scores) {
+        for(int score : scores) {
             System.out.printf("%2d", score);
         }
         System.out.println();
         
         Arrays.fill(scores, 60);
-        for(var score : scores) {
+        for(int score : scores) {
+            System.out.printf("%3d", score);
+        }
+
+        for(int i = 0; i < scores.length; i++) {
+            scores[i] -= 30;
+        }
+
+        for(int score : scores) {
             System.out.printf("%3d", score);
         }
     }
