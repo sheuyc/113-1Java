b8d0032426d080ad83f776b95b317e290e5e8125
sheuyc <167731527+sheuyc@users.noreply.github.com>
2024-10-18 17:45:35 +0900

    2024-10-18

--- 2024-10-18/HW1/src/Time1.java
+++ 2024-10-18/HW1/src/Time1.java
@@ -1,15 +1,92 @@
 public class Time1 {
-    //時(hour)
-    //分(minute)
-    //秒(second)
+
+    private int hour;
+    private int minute;
+    private int second;
 
     //方法：建構子(設定時、分、秒的初始值，需檢查資料是否格式正確)
+    public Time1(int hour, int minute, int second) {
+        setHour(hour);
+        setMinute(minute);
+        setSecond(second);
+    }
 
-    //方法：是需要加入get/set
+    // 設定時
+    public void setHour(int hour) {
+        if (hour < 0 || hour > 23) {
+            throw new IllegalArgumentException("小時必須在 0 到 23 之間");
+        }
+        this.hour = hour;
+    }
 
-    //方法：tick (時間增加一秒)
+    // 取得時
+    public int getHour() {
+        return hour;
+    }
 
-    //方法：print Universal (以24小時制輸出完整時間 15:54:04)
+    // 設定分
+    public void setMinute(int minute) {
+        if (minute < 0 || minute > 59) {
+            throw new IllegalArgumentException("分鐘必須在 0 到 59 之間");
+        }
+        this.minute = minute;
+    }
 
-    //方法：print Standard (以12小時制輸出完整時間 03:54:04 PM)
+    // 取得秒
+    public int getSecond() {
+        return second;
+    }
+
+    // 設定秒
+    public void setSecond(int second) {
+        if (second < 0 || second > 59) {
+            throw new IllegalArgumentException("秒數必須在 0 到 59 之間");
+        }
+        this.second = second;
+    }
+
+    // 取得分
+    public int getMinute() {
+        return minute;
+    }
+
+    //方法：tick (增加時間)
+    public void tick(int time) {
+        if (time > 0){
+            second += time;
+            while (second >= 60 || minute >= 60 ||hour >= 24){
+                if (second >= 60) {
+                    second = second - 60;
+                    minute++;
+                    if (minute >= 60) {
+                        minute = minute - 60;
+                        hour++;
+                        if (hour >= 24) {
+                            hour = hour - 24;
+                        }
+                    }
+                }
+            }
+            System.out.println("秒");
+            System.out.println("過了"+time+"秒後：");
+        }
+        else {
+            System.out.println("時間不為正，來亂的！");
+        }
+    }
+
+    // 以24小時制輸出完整時間
+    public String printUniversal() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    // 以12小時制輸出完整時間
+    public String printStandard() {
+        int standardHour = hour % 12;
+        if (standardHour == 0) {
+            standardHour = 12;
+        }
+        String period = hour < 12 ? "AM" : "PM";
+        return String.format("%02d:%02d:%02d %s", standardHour, minute, second, period);
+    }
 }
--- 2024-10-18/HW1/src/Time1Test.java
+++ 2024-10-18/HW1/src/Time1Test.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class Time1Test {
     public static void  main(String[] args){
-        //Time1 time = new...
-
+        //設定起始時間
+        Time1 time = new Time1(12, 54, 4);
         //呼叫print Universal()
-
+        System.out.println("24小時制：" + time.printUniversal());
         //呼叫print Standard()
+        System.out.println("12小時制：" + time.printStandard());
 
         //使用者輸入時間過了幾秒，用迴圈呼叫tick
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("增加");
+        time.tick(input.nextInt());
 
         //呼叫print Universal()
-
+        System.out.println("24小時制：" + time.printUniversal());
         //呼叫print Standard()
+        System.out.println("12小時制：" + time.printStandard());
     }
 }
