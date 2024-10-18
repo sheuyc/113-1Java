1df88597bb1b5a3ccb592a071329f2729c876677
sheuyc <167731527+sheuyc@users.noreply.github.com>
2024-10-18 12:00:26 +0800

    2024-10-18

---
+++ 2024-10-18/HW1/src/Time1.java
@@ -0,0 +1,15 @@
+public class Time1 {
+    //時(hour)
+    //分(minute)
+    //秒(second)
+
+    //方法：建構子(設定時、分、秒的初始值，需檢查資料是否格式正確)
+
+    //方法：是需要加入get/set
+
+    //方法：tick (時間增加一秒)
+
+    //方法：print Universal (以24小時制輸出完整時間 15:54:04)
+
+    //方法：print Standard (以12小時制輸出完整時間 03:54:04 PM)
+}
---
+++ 2024-10-18/HW1/src/Time1Test.java
@@ -0,0 +1,15 @@
+public class Time1Test {
+    public static void  main(String[] args){
+        //Time1 time = new...
+
+        //呼叫print Universal()
+
+        //呼叫print Standard()
+
+        //使用者輸入時間過了幾秒，用迴圈呼叫tick
+
+        //呼叫print Universal()
+
+        //呼叫print Standard()
+    }
+}
