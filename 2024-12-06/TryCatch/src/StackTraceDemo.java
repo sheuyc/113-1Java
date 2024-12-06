af6bdba426ea75cf1fa1520b2f86610999b1ca7b
sheuyc <167731527+sheuyc@users.noreply.github.com>
2024-12-06 11:08:02 +0800

    2024-12-06

---
+++ 2024-12-06/TryCatch/src/StackTraceDemo.java
@@ -0,0 +1,22 @@
+public class StackTraceDemo {
+    public static void main(String[] args) {
+        try {
+            c();
+        } catch(NullPointerException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    static void c() {
+        b();
+    }
+
+    static void b() {
+        a();
+    }
+
+    static String a() {
+        String text = null;
+        return text.toUpperCase();
+    }
+}
