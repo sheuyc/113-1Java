aa934ed10428e34c41e148d532321e320b7dcb1c
sheuyc <167731527+sheuyc@users.noreply.github.com>
2024-12-06 11:54:43 +0800

    2024-12-06

--- 2024-12-06/TryCatch/src/MutiException.java
+++ 2024-12-06/TryCatch/src/MutiException.java
@@ -3,17 +3,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class MutiException {
-    int[] number = {1, 2, 3};
-    try {
-        File file = new File("nonexistentfile.txt");
-        FileReader reader = new FileReader(file);
-        System.out.println(number[5]);
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        try {
+            File file = new File("nonexistentfile.txt");
+            FileReader reader = new FileReader(file);
+            System.out.println(number[5]);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("處裡未減例外：" + e.getMessage());
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("處裡未減例外：" + e.getMessage());
+        }
     }
-    catch (FileNotFoundException e) {
-        System.out.println("處裡未減例外：" + e.getMessage());
-    }
-    catch (ArrayIndexOutOfBoundsException e) {
-        System.out.println("處裡未減例外：" + e.getMessage());
-    }
-
 }
