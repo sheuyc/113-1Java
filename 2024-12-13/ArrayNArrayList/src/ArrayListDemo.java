af62cd323b24433de409038951262e20c1cbe6d9
sheuyc <167731527+sheuyc@users.noreply.github.com>
2024-12-13 17:13:09 +0900

    2024-12-13

--- 2024-12-13/ArrayNArrayList/src/ArrayListDemo.java
+++ 2024-12-13/ArrayNArrayList/src/ArrayListDemo.java
@@ -10,9 +10,38 @@ public class ArrayListDemo {
         list.add("Banana");
         list.add("Cherry");
 
+        //顯示所有元素
         System.out.println("ArrayList內容：" + list);
 
+        //取得特定索引元素
         String fruit = list.get(1);
         System.out.println("第2個元素是：" + fruit);
+
+        //修訂特定索引元素
+        list.set(1,"Blueberry");
+        System.out.println("更新後的ArrayList：" + list);
+
+        //刪除元素
+        list.remove(0);
+        System.out.println("刪除後的ArrayList：" + list);
+
+        //檢查大小
+        int size = list.size();
+        System.out.println("ArrayList大小：" + size);
+
+        //判斷是剖包含定元素
+        boolean containsCherry = list.contains("Cherry");
+        System.out.println("ArrayList是否包含'Cherry'？" + containsCherry);
+
+        //使用迴圈訪問所有元素
+        if (!list.isEmpty()) {
+            System.out.println("適用迴圈訪問元素：");
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+        }
+        else {
+            System.out.println("ArrayList是空的：");
+        }
     }
 }
